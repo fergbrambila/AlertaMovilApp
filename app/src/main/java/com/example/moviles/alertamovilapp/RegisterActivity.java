53cffc143ee1cbddd850d42709b022b407988aa7
Fernando Garza <fergbrambila@gmail.com>
2015-11-27 22:16:23 -0600

    Validaciones Registrar

--- app/src/main/java/com/example/moviles/alertamovilapp/RegisterActivity.java
+++ app/src/main/java/com/example/moviles/alertamovilapp/RegisterActivity.java
@@ -25,11 +25,19 @@ public class RegisterActivity extends AppCompatActivity {
     private static EditText _apellido;
     private static EditText _mail;
     private static EditText _password;
+    private static EditText _passwordConfirmar;
     private static EditText _numcelular;
     private static EditText _dia;
     private static EditText _mes;
     private static EditText _anio;
     private static String spinnerCiudad;
+    private static String nom;
+    private static String ape;
+    private static String mai;
+    private static String pas;
+    private static String pasC;
+    private static String num;
+    private static String fec;
 
 
     @Override
@@ -93,50 +101,107 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void onButtonListener(){
+
         _btn_signup = (Button)findViewById(R.id.btn_signup);
         _btn_signup.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {
-                        _nombre = (EditText)findViewById(R.id.nombre);
-                        _apellido = (EditText)findViewById(R.id.apellido);
-                        _mail = (EditText)findViewById(R.id.email);
-                        _password = (EditText)findViewById(R.id.password);
-                        _numcelular = (EditText)findViewById(R.id.numerocelular);
-                        _dia = (EditText)findViewById(R.id.fechaDia);
-                        _mes =(EditText)findViewById(R.id.fechaMes);
-                        _anio =(EditText)findViewById(R.id.fechaAnio);
-
-                        final String nom = _nombre.getText().toString();
-                        final String ape = _apellido.getText().toString();
-                        final String mai = _mail.getText().toString();
-                        final String pas = _password.getText().toString();
-                        final String num = _numcelular.getText().toString();
-                        String fec = _dia.getText().toString() +  "/" + _mes.getText().toString() +  "/" + _anio.getText().toString();
-                        fec = "02/05/2012";
-
-
-                        new RegistrarTask(new RegistrarTask.RegistrarCallback() {
-                            @Override
-                            public void onSuccess() {
-                                Toast.makeText(getBaseContext(), "Registro Creado", Toast.LENGTH_LONG).show();
-                                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                                startActivity(intent);
-                            }
-
-                            @Override
-                            public void onFail() {
-                                Toast.makeText(getBaseContext(), "Fallo el Registro - vuelvalo a intentar", Toast.LENGTH_LONG).show();
-                                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                                startActivity(intent);
-                            }
-                        }).execute(mai, pas, nom, ape, num, fec, spinnerCiudad);
-
+                        registrar();
                     }
                 }
         );
     }
 
-    public void login () {
+    public void registrar () {
+        Log.d("register", "registrar");
 
+        _nombre = (EditText)findViewById(R.id.name);
+        _apellido = (EditText)findViewById(R.id.apellido);
+        _mail = (EditText)findViewById(R.id.email);
+        _password = (EditText)findViewById(R.id.password);
+        _passwordConfirmar = (EditText)findViewById(R.id.confirmpassword);
+        _numcelular = (EditText)findViewById(R.id.numerocelular);
+        _anio =(EditText)findViewById(R.id.fechaAnio);
+
+        nom = _nombre.getText().toString();
+        ape = _apellido.getText().toString();
+        mai = _mail.getText().toString();
+        pas = _password.getText().toString();
+        pasC = _passwordConfirmar.getText().toString();
+        num = _numcelular.getText().toString();
+        fec = _anio.getText().toString();
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        new RegistrarTask(new RegistrarTask.RegistrarCallback() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getBaseContext(), "Registro Creado", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onFail() {
+                Toast.makeText(getBaseContext(), "Fallo el Registro - vuelvalo a intentar", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        }).execute(mai, pas, nom, ape, num, fec, spinnerCiudad);
     }
+
+    private void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Rellenar los campos adecuadamente", Toast.LENGTH_LONG).show();
+    }
+
+    private boolean validate() {
+        boolean valid = true;
+
+        if (mai.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(mai).matches()){
+            _mail.setError("Ingresar un correo correcto");
+            valid = false;
+        }
+
+        if (pas.isEmpty()){
+            _password.setError("Ingresar Contrasena");
+            valid = false;
+        }
+
+        if(!pas.equals(pasC)){
+            _passwordConfirmar.setError("La contrasena no concuerda");
+            valid = false;
+        }
+
+        if (nom.isEmpty()){
+            _nombre.setError("Ingresar Nombre");
+            valid = false;
+        }
+
+        if (ape.isEmpty()){
+            _apellido.setError("Ingresar Apellido");
+            valid = false;
+        }
+
+        if(valid == true && fec.isEmpty()){
+            _anio.setText("");
+        }
+
+        if(!fec.isEmpty()){
+            int iFecha = Integer.parseInt("fec");
+            if(!(iFecha > 1900 && iFecha <2020)){
+                _anio.setError("Año invalido");
+            }
+        }
+
+        if (valid == true && num.isEmpty()){
+            _numcelular.setText("");
+        }
+
+        return valid;
+    }
+
+
 }
